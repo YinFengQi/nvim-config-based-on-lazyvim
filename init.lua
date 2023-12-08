0395c619060875449d84dfd4dd3aca3a1548cec6
mny <mny23@mails.tsinghua.edu.cn>
2023-12-08 20:46:48 +0800

    2023.12.8

--- init.lua
+++ init.lua
@@ -6,6 +6,8 @@ require("tokyonight").setup({
   on_colors = function(colors)
     local util = require("tokyonight.util")
     colors.comment = util.lighten(colors.comment, 0.6)
+    colors.bg_highlight = util.lighten(colors.bg_highlight, 0.95)
+    colors.fg_gutter = util.lighten(colors.fg_gutter, 0.9)
     colors.dark5 = util.darken(colors.blue5, 0.8)
   end,
 })
