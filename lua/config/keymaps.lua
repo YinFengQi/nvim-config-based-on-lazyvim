f8a888b601484235a7868a2a10d04e327913a8d2
mny <mny23@mails.tsinghua.edu.cn>
2024-01-30 17:52:59 +0800

    增加了绘图相关的东西

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -12,7 +12,7 @@ keymap.set({ "i", "s" }, "kj", "<Esc>")
 keymap.set("n", "<leader>h", "<cmd>noh<cr>", { desc = "no highlight" })
 
 -- keymap.set("i", "<c-c>", "<cmd>lua require('luasnip.extras.select_choice')()<cr>")
--- keymap.set("i", "<c-s>", "<cmd>lea require('lea snip.extras.select_choice')()<Cr>")
+-- keymap.set("i", "<c-s>", "<cmd>lua require('luasnip.extras.select_choice')()<Cr>")
 keymap.set({ "i", "s" }, "<c-u>", "<cmd>lua require('luasnip.extras.select_choice')()<cr>")
 keymap.set({ "i", "s" }, "<c-n>", "<Plug>luasnip-next-choice")
 keymap.set({ "i", "s" }, "<c-p>", "<Plug>luasnip-prev-choice")
@@ -28,3 +28,16 @@ keymap.set("i", "<c-e>", "<esc><c-e>a")
 -- 下面的是来自 Gilles Castel 的快速拼写修正的 keymap,
 -- "It basically jumps to the previous spelling mistake [s, then picks the first suggestion 1z=, and then jumps back `]a. The <c-g>u in the middle make it possible to undo the spelling correction quickly."
 keymap.set("i", "<c-l>", "<c-g>u<Esc>[s1z=`]a<c-g>u")
+
+-- 下面是来自 Gilles Castel 的图片插入代码, 见
+keymap.set(
+  "i",
+  "<C-f>",
+  [[ <Esc>: silent exec '.!inkscape-figures create "'.getline('.').'" "'.b:vimtex.root.'/figures/"'<CR><CR>:w<CR>]]
+)
+
+keymap.set(
+  "n",
+  "<C-f>",
+  [[ : silent exec '!inkscape-figures edit "'.b:vimtex.root.'/figures/" > /dev/null 2>&1 &'<CR><CR>:redraw!<CR>]]
+)
